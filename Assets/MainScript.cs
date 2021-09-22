16e885b82dfaea36404789a37377fd6a6fe71a24
Tim Fowler <30913434+timmydasta@users.noreply.github.com>
2021-09-22 17:06:15 +1200

    Reading reports in app
    -Started working on being able to read reports in the app.
    - Added the ability to cancel creating a new report

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI;
 using UnityEngine.Networking; using SimpleJSON; using System.Linq;
+using UnityEngine.EventSystems;
 
 public class MainScript : MonoBehaviour{
     
@@ -16,60 +17,69 @@ public class MainScript : MonoBehaviour{
 	public string correctSafetyZone = "";
 	public string correctFitForPurpose = "";
 	
-	private Transform reportForm;
+	private Transform newReportForm;
 	private Dropdown trainerDropdown;
 	private Dropdown traineeDropdown;
 	private Dropdown siteDropdown;
 	
+	private Transform viewReports;
+	
+	private Transform report;
+	
 	public class Trainer{
 		public string name;
-		public Trainer(string newName){ name = newName; }
+		public string branch;
+		public Trainer(string newName, string newBranch){ name = newName; branch = newBranch; }
 	}
 	public List<Trainer> trainers = new List<Trainer>();
 	
 	public class Trainee{
 		public string name;
-		public Trainee(string newName){ name = newName; }
+		public string branch;
+		public Trainee(string newName, string newBranch){ name = newName; branch = newBranch; }
 	}
 	public List<Trainee> trainees = new List<Trainee>();
+	
+	public class Report{
+		public string date;
+		public string trainer;
+		public string trainee;
+		public string site;
+		public string branch;
+		
+		public Report(string newDate, string newTrainer, string newTrainee, string newSite, string newBranch){
+			date = newDate;
+			trainer = newTrainer;
+			trainee = newTrainee;
+			site = newSite;
+			branch = newBranch; 
+		}
+	}
+	
+	public List<Report> reports = new List<Report>();
 	
 	// Start is called before the first frame update
     void Start(){
 		
 		SetDate();
 		
-		reportForm = GameObject.Find("NewReportForm").transform;
+		newReportForm = GameObject.Find("NewReportForm").transform;
 	  	trainerDropdown = GameObject.Find("Trainer_Dropdown").GetComponent<Dropdown>();	traineeDropdown = GameObject.Find("Trainee_Dropdown").GetComponent<Dropdown>();	siteDropdown = GameObject.Find("Site_Dropdown").GetComponent<Dropdown>();
-		GameObject.Find("Trainee_Label").GetComponent<Text>().text = GameObject.Find("Trainer_Label").GetComponent<Text>().text = GameObject.Find("Site_Label").GetComponent<Text>().text = "";
+		newReportForm.localScale = new Vector2(0, 0);
 		
-		StartCoroutine(FindData());
+		viewReports = GameObject.Find("ViewReports").transform;
+		viewReports.localScale = new Vector2(0, 0);
+		
+		report = GameObject.Find("Report").transform;
+		report.localScale = new Vector2(0, 0);
+		
+		StartCoroutine(FindStaffData());
+		StartCoroutine(FindReports());
     }
 
-    // Update is called once per frame
-    void Update(){
-    }
+	public IEnumerator FindStaffData(){
 		
-	public void createNewForm(){
-		reportForm.localScale = new Vector2(1, 1);
-			
-		date = dateReport = trainer = trainee = site = "";
-		correctSigns = correctTaperLength = correctTaperSpacing = correctSiteSpacing = correctSafetyZone = correctFitForPurpose = "";
-		
-		GameObject.Find("Trainee_Label").GetComponent<Text>().text = GameObject.Find("Trainer_Label").GetComponent<Text>().text = GameObject.Find("Site_Label").GetComponent<Text>().text = "";
-		
-		GameObject.Find("CorrectSigns_Yes").GetComponent<Image>().color =  GameObject.Find("CorrectSigns_No").GetComponent<Image>().color =	 GameObject.Find("CorrectSigns_NA").GetComponent<Image>().color = 		
-		GameObject.Find("CorrectTaper_Yes").GetComponent<Image>().color =  GameObject.Find("CorrectTaper_No").GetComponent<Image>().color =  GameObject.Find("CorrectTaper_NA").GetComponent<Image>().color = 		
-		GameObject.Find("TaperSpacing_Yes").GetComponent<Image>().color =  GameObject.Find("TaperSpacing_No").GetComponent<Image>().color =	 GameObject.Find("TaperSpacing_NA").GetComponent<Image>().color = 		
-		GameObject.Find("SiteSpacing_Yes").GetComponent<Image>().color =   GameObject.Find("SiteSpacing_No").GetComponent<Image>().color =	 GameObject.Find("SiteSpacing_NA").GetComponent<Image>().color = 		
-		GameObject.Find("SafetyZone_Yes").GetComponent<Image>().color =    GameObject.Find("SafetyZone_No").GetComponent<Image>().color =	 GameObject.Find("SafetyZone_NA").GetComponent<Image>().color =
-		GameObject.Find("FitForPurpose_Yes").GetComponent<Image>().color = GameObject.Find("FitForPurpose_No").GetComponent<Image>().color = GameObject.Find("FitForPurpose_NA").GetComponent<Image>().color = new Color32(137, 137, 137, 255);
-		
-		SetDate();
-	}
-		
-	public IEnumerator FindData(){
-		
-		UnityWebRequest www = UnityWebRequest.Get("https://ttmtrainingapp-default-rtdb.firebaseio.com/Wilsons.json");
+		UnityWebRequest www = UnityWebRequest.Get("https://ttmtrainingapp-default-rtdb.firebaseio.com/Wilsons/Staff.json");
 		using (www){ yield return www.SendWebRequest();
 			
 			var all = JSON.Parse(www.downloadHandler.text);
@@ -77,14 +87,14 @@ public class MainScript : MonoBehaviour{
 			string key = "Staff_0"; int i = 1;
 			if(i >= 10){ key = "Staff_"; }
 
-			while(i <= all["Staff"].Count){
+			while(i <= all.Count){
 				
 				if(i >= 10){ key = "Staff_"; }
 				
-				trainees.Add(new Trainee(all["Staff"][key+i]["Name"].Value));
+				trainees.Add(new Trainee(all[key+i]["Name"].Value, all[key+i]["Branch"].Value));
 				
-				if(bool.Parse(all["Staff"][key+i]["Trainer"].Value)){
-					trainers.Add(new Trainer(all["Staff"][key+i]["Name"].Value));
+				if(bool.Parse(all[key+i]["Trainer"].Value)){
+					trainers.Add(new Trainer(all[key+i]["Name"].Value, all[key+i]["Branch"].Value));
 				}
 				
 				i++;
@@ -109,7 +119,58 @@ public class MainScript : MonoBehaviour{
 			traineeTemplate.sizeDelta = new Vector2(traineeTemplate.sizeDelta.x, trainees.Count * 80f);
 		}
 	}
-
+		
+	public IEnumerator FindReports(){
+		UnityWebRequest www = UnityWebRequest.Get("https://ttmtrainingapp-default-rtdb.firebaseio.com/Wilsons/Reports.json");
+		using (www){ yield return www.SendWebRequest();
+			
+			var all = JSON.Parse(www.downloadHandler.text);
+			
+			
+			JSONNode.KeyEnumerator keysNelson = all["Nelson"].Keys;
+			while(keysNelson.MoveNext()){ 
+				reports.Add(new Report(	
+					all["Nelson"][keysNelson.Current]["Date"].Value,
+					all["Nelson"][keysNelson.Current]["Trainer"].Value,
+					all["Nelson"][keysNelson.Current]["Trainee"].Value,
+					all["Nelson"][keysNelson.Current]["Site"].Value,
+					"Nelson"
+				));	
+			}
+			
+			JSONNode.KeyEnumerator keysChch = all["Christchurch"].Keys;
+			while(keysChch.MoveNext()){  
+				reports.Add(new Report(
+					all["Christchurch"][keysChch.Current]["Date"].Value,
+					all["Christchurch"][keysChch.Current]["Trainer"].Value,
+					all["Christchurch"][keysChch.Current]["Trainee"].Value,
+					all["Christchurch"][keysChch.Current]["Site"].Value,
+					"Christchurch"
+				));	  
+			}
+		}
+	}
+		
+	#region New Report Forms
+	
+	public void CreateNewForm(){
+		newReportForm.localScale = new Vector2(1, 1);
+			
+		date = dateReport = trainer = trainee = site = "";
+		correctSigns = correctTaperLength = correctTaperSpacing = correctSiteSpacing = correctSafetyZone = correctFitForPurpose = "";
+		
+		GameObject.Find("Trainee_Label").GetComponent<Text>().text = GameObject.Find("Trainer_Label").GetComponent<Text>().text = GameObject.Find("Site_Label").GetComponent<Text>().text = "";
+		
+		GameObject.Find("CorrectSigns_Yes").GetComponent<Image>().color =  GameObject.Find("CorrectSigns_No").GetComponent<Image>().color =	 GameObject.Find("CorrectSigns_NA").GetComponent<Image>().color = 		
+		GameObject.Find("CorrectTaper_Yes").GetComponent<Image>().color =  GameObject.Find("CorrectTaper_No").GetComponent<Image>().color =  GameObject.Find("CorrectTaper_NA").GetComponent<Image>().color = 		
+		GameObject.Find("TaperSpacing_Yes").GetComponent<Image>().color =  GameObject.Find("TaperSpacing_No").GetComponent<Image>().color =	 GameObject.Find("TaperSpacing_NA").GetComponent<Image>().color = 		
+		GameObject.Find("SiteSpacing_Yes").GetComponent<Image>().color =   GameObject.Find("SiteSpacing_No").GetComponent<Image>().color =	 GameObject.Find("SiteSpacing_NA").GetComponent<Image>().color = 		
+		GameObject.Find("SafetyZone_Yes").GetComponent<Image>().color =    GameObject.Find("SafetyZone_No").GetComponent<Image>().color =	 GameObject.Find("SafetyZone_NA").GetComponent<Image>().color =
+		GameObject.Find("FitForPurpose_Yes").GetComponent<Image>().color = GameObject.Find("FitForPurpose_No").GetComponent<Image>().color = GameObject.Find("FitForPurpose_NA").GetComponent<Image>().color = new Color32(137, 137, 137, 255);
+		
+		SetDate();
+	}
+		
 	public void SetDate(){
 		System.DateTime now = System.DateTime.Now; 
 		date = now.Day + "/" + now.Month + "/" + now.Year;
@@ -118,7 +179,6 @@ public class MainScript : MonoBehaviour{
 	}
 	
 	public void SetTrainer(){
-		
 		trainer = trainerDropdown.options[trainerDropdown.value].text;
 	}
 	
@@ -185,6 +245,17 @@ public class MainScript : MonoBehaviour{
 		GameObject.Find($"FitForPurpose_{selected}").GetComponent<Image>().color = new Color32(200, 200, 200, 255);
 	}
 
+	public void Cancel(){
+		
+		GameObject.Find("Trainer").GetComponent<Image>().sprite = GameObject.Find("Trainee").GetComponent<Image>().sprite = Resources.Load<Sprite>("IMG/WilsonsSmallDropDown");
+		GameObject.Find("Site").GetComponent<Image>().sprite = Resources.Load<Sprite>("IMG/WilsonsLargeDropDown");
+		GameObject.Find("CorrectSigns").GetComponent<Image>().sprite = GameObject.Find("CorrectTaper").GetComponent<Image>().sprite = 
+		GameObject.Find("TaperSpacing").GetComponent<Image>().sprite = GameObject.Find("SiteSpacing").GetComponent<Image>().sprite = 
+		GameObject.Find("SafetyZone").GetComponent<Image>().sprite = GameObject.Find("FitForPurpose").GetComponent<Image>().sprite = Resources.Load<Sprite>("IMG/WilsonsLarge");
+		
+		newReportForm.localScale = new Vector2(0, 0);
+	}
+
 	public void Submit(){
 		
 		GameObject.Find("Trainer").GetComponent<Image>().sprite = GameObject.Find("Trainee").GetComponent<Image>().sprite = Resources.Load<Sprite>("IMG/WilsonsSmallDropDown");
@@ -207,13 +278,14 @@ public class MainScript : MonoBehaviour{
 			
 		}
 		else {
-			Debug.Log("All Correct");
 			StartCoroutine(SendForm());
-			reportForm.localScale = new Vector2(0, 0);
+			newReportForm.localScale = new Vector2(0, 0);
 		}
 	}
 
 	public IEnumerator SendForm(){
+				
+		string branch = trainers[trainers.FindIndex(x => x.name == trainer)].branch;
 		
 		string json = 	"{" +
 							$"\"Date\":\"{date}\",\"Trainer\":\"{trainer}\",\"Trainee\":\"{trainee}\",\"Site\":\"{site}\"," +
@@ -223,8 +295,49 @@ public class MainScript : MonoBehaviour{
 						"}";
 					
 		byte[] data = System.Text.Encoding.UTF8.GetBytes(json);
-		UnityWebRequest put = UnityWebRequest.Put($"https://ttmtrainingapp-default-rtdb.firebaseio.com/Wilsons/Reports/{dateReport}_{trainer}_{trainee}_{site}.json", data);
+		UnityWebRequest put = UnityWebRequest.Put($"https://ttmtrainingapp-default-rtdb.firebaseio.com/Wilsons/Reports/{branch}/{dateReport}_{trainer}_{trainee}_{site}.json", data);
 		yield return put.SendWebRequest();
 						
 	}
+
+	#endregion New Report Forms
+
+	#region View All Reports 
+	
+	public void OpenReports(){
+		viewReports.localScale = new Vector2(1, 1);
+		
+		int i = 0;
+		while(i < reports.Count){
+			Transform parent = GameObject.Find("Reports").transform;
+			GameObject newSmallReport = Instantiate(Resources.Load<GameObject>("Prefabs/ReportSmall"), new Vector2(parent.position.x, (parent.position.y - 85f) - (180 * i)), Quaternion.identity, parent);
+			newSmallReport.name = $"ReportSmall_{i}";
+			newSmallReport.GetComponent<Button>().onClick.AddListener(delegate{ OpenReport(); });
+			
+			newSmallReport.transform.Find("Date").GetComponent<Text>().text = reports[i].date;
+			newSmallReport.transform.Find("Trainer").GetComponent<Text>().text = reports[i].trainer;
+			newSmallReport.transform.Find("Trainee").GetComponent<Text>().text = reports[i].trainee;
+			newSmallReport.transform.Find("Site").GetComponent<Text>().text = reports[i].site;
+			
+			i++;
+		}
+	}
+	
+	public void Back(){
+		viewReports.localScale = new Vector2(0, 0);
+	}
+	
+	#endregion View All Reports
+	
+	#region View Detailed Report
+		public void OpenReport(){
+			Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+			report.localScale = new Vector2(1, 1);
+		}
+	
+		public void CloseReport(){
+			report.localScale = new Vector2(0, 0);
+		
+		}
+	#endregion View Detailed Report
 }
